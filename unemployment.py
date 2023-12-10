1c3342a468a3c523f5368a7340882a23f09262c0
UMAMA <149988908+umamamama@users.noreply.github.com>
2023-12-09 18:49:14 -0600

    Update unemployment.py

--- unemployment.py
+++ unemployment.py
@@ -1,7 +1,8 @@
+# Import Pandas library
 import pandas as pd
 
 # Read the original data from the CSV file
-file_path = r"C:\Users\moham\Downloads\df_sex_unemployment_rates.csv"
+file_path = r"<file_path>"
 nojob = pd.read_csv(file_path)
 
 # Display basic information about the original DataFrame
@@ -17,7 +18,7 @@ null_rows = nojob.loc[nojob.isnull().any(axis=1)]
 print("Dates with null values")
 print(null_rows[['date'] + list(null_rows.columns[null_rows.isnull().any()])])
 
-# Select a subset of columns to keep in the DataFrame
+# Select columns of men/women over the age of 20 to keep in the DataFrame
 cols_to_keep = ['date', 'men_20_24_rate', 'women_20_24_rate', 'men_25plus_rate', 'women_25plus_rate',
                 'men_25_34_rate', 'women_25_34_rate', 'men_35_44_rate', 'women_35_44_rate',
                 'men_45_54_rate', 'women_45_54_rate', 'men_55plus_rate', 'women_55plus_rate']
@@ -34,7 +35,8 @@ nojob['20_plus_men_rate'] = nojob[['men_20_24_rate', 'men_25plus_rate', 'men_25_
 nojob['20_plus_women_rate'] = nojob[['women_20_24_rate', 'women_25plus_rate', 'women_25_34_rate',
                                       'women_35_44_rate', 'women_45_54_rate', 'women_55plus_rate']].mean(axis=1, skipna=True)
 
-# Melt the DataFrame to convert from wide to long format
+
+# Melt the DataFrame to convert from wide to long format to easily visualize data in Tableau
 melted_df = pd.melt(nojob, id_vars=['date'], value_vars=['men_20_24_rate', 'women_20_24_rate',
                                                         'men_25_34_rate', 'women_25_34_rate',
                                                         'men_35_44_rate', 'women_35_44_rate',
@@ -53,7 +55,6 @@ print(melted_df.info())
 # Display the first 20 rows of the melted DataFrame
 print("First 20 rows of the melted DataFrame:")
 print(melted_df.head(20))
-
 
 
 # Save the final melted DataFrame to a new CSV file
